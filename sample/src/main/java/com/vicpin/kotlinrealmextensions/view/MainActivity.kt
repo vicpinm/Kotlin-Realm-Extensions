e5227e5547da31f819e36dc08f6af3b29f0d3992
Victor <victor.pineda@digio.es>
2017-01-12 17:05:32 +0100

    version 1.0.2

--- sample/src/main/java/com/vicpin/kotlinrealmextensions/view/MainActivity.kt
+++ sample/src/main/java/com/vicpin/kotlinrealmextensions/view/MainActivity.kt
@@ -2,11 +2,7 @@ package com.vicpin.kotlinrealmextensions.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.vicpin.kotlinrealmextensions.R
-import com.vicpin.krealmextensions.firstItemAsync
-import com.vicpin.krealmextensions.model.TestEntityPK
-import com.vicpin.krealmextensions.save
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,14 +14,7 @@ class MainActivity : AppCompatActivity() {
         //See tests for complete usage
         //***********************************
 
-        populateDBWithTestEntityPK(5)
-        TestEntityPK().firstItemAsync {
-            it -> Log.d("Result", it?.name)
-        }
 
     }
 
-    private fun populateDBWithTestEntityPK(numItems : Int){
-        (0..numItems - 1).forEach { TestEntityPK(it.toLong()).save() }
-    }
 }
